dbfd6e51db5e03ba7fe44f0b91b2e89b4eb7b410
Sachin Singh <ssingh259@sapient.com>
2019-02-20 11:01:54 +0530

    Updated folder hierarchy

---
+++ src/components/templates/Content/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => (
+    <div className="wwn-content">
+        <div className="wwn-container">
+            This is body
+        </div>
+    </div>
+);
+
+export default Content;
